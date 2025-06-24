1652abedefe373654cfe30a10d2d3d0493166039
jaypal1184 <jaypalpatel1184@gmail.com>
2025-06-24 14:56:45 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUHzUxEJz3ZSz1Og3ABnCSW4PQ2ztN3s0",
+  authDomain: "youbook-847b0.firebaseapp.com",
+  projectId: "youbook-847b0",
+  storageBucket: "youbook-847b0.firebasestorage.app",
+  messagingSenderId: "605919819786",
+  appId: "1:605919819786:web:34deeb38ec08bd03f347f3",
+  measurementId: "G-FWPQC4TKTY"
+};
